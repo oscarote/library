0f7bd261afe34a47b9babd71dd6fdb1f1568a443
Oscar Otero <oskar.oc@gmail.com>
2022-10-27 13:16:20 +0200

    Edit script.js and style.css
    Add CSS styles for the modal window. Modify buttons and
    make cards display as grid. Create functions so an user
    can input a book and it will show in the index.

--- script.js
+++ script.js
@@ -7,39 +7,68 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = function() {
         let readMsg = null;
-        read === "yes" ? (readMsg = "already read") : (readMsg = "not read yet");
+        read === "on" ? (readMsg = "already read") : (readMsg = "not read yet");
         return `${title} by ${author}, ${pages} pages, ${readMsg}`
     }
 }
-// Testing
-let hobbit = new Book("The Hobbit", "JRR Tolkien", 524, "yes");
-let edsla = new Book("El Señor de los Anillos", "JRR Tolkien", 5566, "no");
 
-// Library
-let myLibrary = [hobbit, edsla];
-
-function addBookToLibrary() {
-    //do stuff
+// Close modal
+function closeModal() {
+    modalDiv.classList.remove("active");
+    overlayDiv.classList.remove("active");
 }
 
 // Document selectors
 const bookCards = document.querySelector(".bookCards");
+const modalDiv = document.getElementById("addBookModal");
+const overlayDiv = document.querySelector(".overlay");
+const addBookBtn = document.getElementById("addBook");
+const submitBtn = document.querySelector("button[type=\"submit\"]");
+
+// Event listeners
+addBookBtn.addEventListener("click", () => {
+    modalDiv.classList.add("active");
+    overlayDiv.classList.add("active");
+});
+
+overlayDiv.addEventListener("click", closeModal);
+
+window.addEventListener("keydown", (e) => { if (e.key === "Escape") closeModal() });
+
+submitBtn.addEventListener("click", addBookToLibrary);
+
+// Library
+let myLibrary = [];
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    myLibrary.push(new Book(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, document.getElementById("isRead").value));
+    console.log(document.getElementById("isRead").value);
+    closeModal();
+    refreshLibrary();
+}
 
 // Show library on index
-for (let book in myLibrary) {
-    const singleCard = document.createElement("div");
-    singleCard.className = "card";
+function refreshLibrary() {
+    bookCards.innerText = "";
+    for (let book in myLibrary) {
+        const singleCard = document.createElement("div");
+        singleCard.className = "card";
 
-    for (const [key, value] of Object.entries(myLibrary[book])) {
-        if (key === "info") break;
-        const h4 = document.createElement("h4");
-        h4.innerText = key;
-        singleCard.appendChild(h4);
+        for (const [key, value] of Object.entries(myLibrary[book])) {
+            if (key === "info") break;
+            const h4 = document.createElement("h4");
+            h4.innerText = key;
+            singleCard.appendChild(h4);
 
-        const para = document.createElement("p");
-        para.innerText = value;
-        singleCard.appendChild(para);
+            const para = document.createElement("p");
+            para.innerText = value;
+            singleCard.appendChild(para);
+        }
+
+        bookCards.appendChild(singleCard);
     }
-
-    bookCards.appendChild(singleCard);
 }
+
+// First load
+refreshLibrary();
