5e505ec41362bd441d8b18ff64537d8b7ece1f91
Oscar Otero <oskar.oc@gmail.com>
2022-10-27 19:23:22 +0200

    Create JS functions
    Add button to each book card to toggle between read ot not
    read. Add button to remove the book from the library. Create
    all JS functions needed for those buttons. Change event
    listener if the input from click to submit.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
+    this.info = function () {
         let readMsg = null;
-        read === "on" ? (readMsg = "already read") : (readMsg = "not read yet");
+        read === "true" ? (readMsg = "already read") : (readMsg = "not read yet");
         return `${title} by ${author}, ${pages} pages, ${readMsg}`
     }
 }
@@ -23,7 +23,8 @@ const bookCards = document.querySelector(".bookCards");
 const modalDiv = document.getElementById("addBookModal");
 const overlayDiv = document.querySelector(".overlay");
 const addBookBtn = document.getElementById("addBook");
-const submitBtn = document.querySelector("button[type=\"submit\"]");
+const formElement = document.getElementById("addBookForm");
+
 
 // Event listeners
 addBookBtn.addEventListener("click", () => {
@@ -35,16 +36,20 @@ overlayDiv.addEventListener("click", closeModal);
 
 window.addEventListener("keydown", (e) => { if (e.key === "Escape") closeModal() });
 
-submitBtn.addEventListener("click", addBookToLibrary);
+formElement.addEventListener("submit", (e) => addBookToLibrary(e));
 
 // Library
 let myLibrary = [];
 
 function addBookToLibrary(e) {
     e.preventDefault();
-    myLibrary.push(new Book(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, document.getElementById("isRead").value));
-    console.log(document.getElementById("isRead").value);
+    const tittle = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("isRead").checked;
+    myLibrary.push(new Book(tittle, author, pages, read));
     closeModal();
+    document.getElementById("addBookForm").reset();
     refreshLibrary();
 }
 
@@ -54,21 +59,57 @@ function refreshLibrary() {
     for (let book in myLibrary) {
         const singleCard = document.createElement("div");
         singleCard.className = "card";
+        singleCard.setAttribute("data-attribute", book);
 
         for (const [key, value] of Object.entries(myLibrary[book])) {
             if (key === "info") break;
-            const h4 = document.createElement("h4");
-            h4.innerText = key;
-            singleCard.appendChild(h4);
+            if (key === "read") {
+                const readBtn = document.createElement("button");
+                if (value === true) {
+                    readBtn.className = "read";
+                    readBtn.innerText = "Read";
+                } else {
+                    readBtn.className = "notRead";
+                    readBtn.innerText = "Not read";
+                }
+                singleCard.appendChild(readBtn);
+                readBtn.addEventListener("click", toggleRead);
+            } else {
+                const h4 = document.createElement("h4");
+                h4.innerText = key;
+                singleCard.appendChild(h4);
 
-            const para = document.createElement("p");
-            para.innerText = value;
-            singleCard.appendChild(para);
+                const para = document.createElement("p");
+                para.innerText = value;
+                singleCard.appendChild(para);
+            }
         }
+
+        const removeBtn = document.createElement("button");
+        removeBtn.className = "removeCard";
+        removeBtn.innerText = "Remove";
+        singleCard.appendChild(removeBtn);
+        removeBtn.addEventListener("click", deleteCard);
 
         bookCards.appendChild(singleCard);
     }
 }
 
-// First load
-refreshLibrary();
+// Card buttons
+function toggleRead() {
+    console.log(this.getAttribute("class"));
+    const dataAtt = this.closest("div").getAttribute("data-attribute");
+    if (this.getAttribute("class") === "read") {
+        myLibrary[dataAtt].read = false;
+    } else {
+        myLibrary[dataAtt].read = true;
+    }
+    refreshLibrary();
+}
+
+function deleteCard() {
+    console.log(this.closest("div").getAttribute("data-attribute"));
+    const dataAtt = this.closest("div").getAttribute("data-attribute");
+    myLibrary.splice(dataAtt, 1);
+    refreshLibrary();
+}
