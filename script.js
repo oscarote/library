342fad819141f8732787e2511defed2153486d89
Oscar Otero <oskar.oc@gmail.com>
2022-10-27 11:46:18 +0200

    Create function to show library as HTML

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Object constructor
 function Book(title, author, pages, read) {
     console.log(this.title);
     this.title = title;
@@ -9,4 +10,36 @@ function Book(title, author, pages, read) {
         read === "yes" ? (readMsg = "already read") : (readMsg = "not read yet");
         return `${title} by ${author}, ${pages} pages, ${readMsg}`
     }
+}
+// Testing
+let hobbit = new Book("The Hobbit", "JRR Tolkien", 524, "yes");
+let edsla = new Book("El Señor de los Anillos", "JRR Tolkien", 5566, "no");
+
+// Library
+let myLibrary = [hobbit, edsla];
+
+function addBookToLibrary() {
+    //do stuff
+}
+
+// Document selectors
+const bookCards = document.querySelector(".bookCards");
+
+// Show library on index
+for (let book in myLibrary) {
+    const singleCard = document.createElement("div");
+    singleCard.className = "card";
+
+    for (const [key, value] of Object.entries(myLibrary[book])) {
+        if (key === "info") break;
+        const h4 = document.createElement("h4");
+        h4.innerText = key;
+        singleCard.appendChild(h4);
+
+        const para = document.createElement("p");
+        para.innerText = value;
+        singleCard.appendChild(para);
+    }
+
+    bookCards.appendChild(singleCard);
 }
